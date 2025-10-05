4a4d258727eeea9ced71254cfcce82fa948d647b
Raniya Javed <raniyajaved4@gmail.com>
2025-10-05 12:44:23 +0530

    first commit

---
+++ src/components/TitileCards/TitleCards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TitleCards.css'
+
+const TitleCards = () => {
+  return (
+    <div className='Titlecards'>
+      
+    </div>
+  )
+}
+
+export default TitleCards
